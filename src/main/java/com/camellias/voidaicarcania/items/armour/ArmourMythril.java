494c2fa3c17e2bf0a6647ec6b19672167ff76e3e
Cammie <inazuma261@gmail.com>
2018-07-22 23:45:07 -0400

    Said I was done, but I'm back at it, still got nothing

--- src/main/java/com/camellias/voidaicarcania/items/armour/ArmourMythril.java
+++ src/main/java/com/camellias/voidaicarcania/items/armour/ArmourMythril.java
@@ -15,9 +15,11 @@ import net.minecraftforge.fml.relauncher.SideOnly;
 
 public class ArmourMythril extends ItemArmor implements IHasModel
 {
-	public ArmourMythril(ArmorMaterial material, int renderIndex, EntityEquipmentSlot equipmentSlot, String name)
+	protected ModelBiped model;
+	
+	public ArmourMythril(ArmorMaterial material, int index, EntityEquipmentSlot slot, String name)
 	{
-		super(material, renderIndex, equipmentSlot);
+		super(material, index, slot);
 		
 		this.setUnlocalizedName(name);         
 		this.setRegistryName(name);
@@ -27,46 +29,15 @@ public class ArmourMythril extends ItemArmor implements IHasModel
 		ModItems.ITEMS.add(this);
 	}
 	
-	@SideOnly(Side.CLIENT)
-    public ModelBiped model;
-	
 	@Override
 	@SideOnly(Side.CLIENT)
 	public ModelBiped getArmorModel(EntityLivingBase entity, ItemStack stack, EntityEquipmentSlot slot, ModelBiped biped)
 	{
 		if(model == null)
 		{
-            if(armorType == slot.HEAD)
-            {
-            	model = new ModelMythrilArmour(1F, true, false, false, false);
-            }
-            
-            else if(armorType == slot.CHEST)
-            {
-            	model = new ModelMythrilArmour(1F, false, true, false, false);
-            }
-            
-            else if(armorType == slot.LEGS)
-            {
-            	model = new ModelMythrilArmour(1F, false, false, true, false);
-            }
-            
-            else if(armorType == slot.FEET)
-            {
-            	model = new ModelMythrilArmour(1F, false, false, false, true);
-            }
-            
-            model.bipedHead.showModel = (armorType == slot.HEAD);
-            model.bipedHeadwear.showModel = (armorType == slot.HEAD);
-            
-            model.bipedBody.showModel = (armorType == slot.CHEST);
-            model.bipedLeftArm.showModel = (armorType == slot.CHEST);
-            model.bipedRightArm.showModel = (armorType == slot.CHEST);
-            
-            model.bipedLeftLeg.showModel = (armorType == slot.LEGS || armorType == slot.FEET);
-            model.bipedRightLeg.showModel = (armorType == slot.LEGS || armorType == slot.FEET);
+            model = new ModelMythrilArmour(slot);
         }
-        
+		
         return model;
 	}
 	
--- src/main/java/com/camellias/voidaicarcania/items/armour/models/ModelMythrilArmour.java
+++ src/main/java/com/camellias/voidaicarcania/items/armour/models/ModelMythrilArmour.java
@@ -1,14 +1,12 @@
 package com.camellias.voidaicarcania.items.armour.models;
 
-import com.camellias.voidaicarcania.Reference;
+import javax.annotation.Nullable;
 
-import net.minecraft.client.Minecraft;
-import net.minecraft.client.model.ModelBiped;
 import net.minecraft.client.model.ModelRenderer;
 import net.minecraft.entity.Entity;
-import net.minecraft.util.ResourceLocation;
+import net.minecraft.inventory.EntityEquipmentSlot;
 
-public class ModelMythrilArmour extends ModelBiped 
+public class ModelMythrilArmour extends ModelArmourBase
 {
     private ModelRenderer HelmetLeft1;
     private ModelRenderer HelmetLeft2;
@@ -38,153 +36,154 @@ public class ModelMythrilArmour extends ModelBiped
     private ModelRenderer LeftLegGuard;
     private ModelRenderer LeftFoot;
     
-    public ModelMythrilArmour(float scale, boolean isHelmet, boolean isChest, boolean isLegs, boolean isBoots)
+    public ModelMythrilArmour(EntityEquipmentSlot slot)
     {
-    	super(scale, 0, 64, 128);
+    	super(slot);
     	
-        this.textureWidth = 64;
-        this.textureHeight = 128;
+        textureWidth = 64;
+        textureHeight = 128;
         
-        this.LeftLegGuard = new ModelRenderer(this, 22, 82);
-        this.LeftLegGuard.setRotationPoint(0.0F, 0.0F, 0.0F);
-        this.LeftLegGuard.addBox(-1.9F, -2.0F, -2.5F, 4, 14, 5, 0.0F);
+        LeftLegGuard = new ModelRenderer(this, 22, 82);
+        LeftLegGuard.setRotationPoint(0.0F, 0.0F, 0.0F);
+        LeftLegGuard.addBox(-1.9F, -2.0F, -2.5F, 4, 14, 5, 0.0F);
         
-        this.RightFoot = new ModelRenderer(this, 40, 93);
-        this.RightFoot.setRotationPoint(0.0F, 0.0F, 0.0F);
-        this.RightFoot.addBox(-2.2F, 6.0F, -3.0F, 4, 6, 6, 0.0F);
+        RightFoot = new ModelRenderer(this, 40, 93);
+        RightFoot.setRotationPoint(0.0F, 0.0F, 0.0F);
+        RightFoot.addBox(-2.2F, 6.0F, -3.0F, 4, 6, 6, 0.0F);
         
-        this.HelmetTop = new ModelRenderer(this, 10, 72);
-        this.HelmetTop.setRotationPoint(0.0F, 0.0F, 0.0F);
-        this.HelmetTop.addBox(-4.0F, -9.0F, -4.0F, 8, 1, 8, 0.0F);
+        HelmetTop = new ModelRenderer(this, 10, 72);
+        HelmetTop.setRotationPoint(0.0F, 0.0F, 0.0F);
+        HelmetTop.addBox(-4.0F, -9.0F, -4.0F, 8, 1, 8, 0.0F);
         
-        this.HelmetFrontLeft1 = new ModelRenderer(this, 34, 70);
-        this.HelmetFrontLeft1.setRotationPoint(0.0F, 0.0F, 0.0F);
-        this.HelmetFrontLeft1.addBox(3.0F, -8.0F, -5.0F, 1, 9, 1, 0.0F);
+        HelmetFrontLeft1 = new ModelRenderer(this, 34, 70);
+        HelmetFrontLeft1.setRotationPoint(0.0F, 0.0F, 0.0F);
+        HelmetFrontLeft1.addBox(3.0F, -8.0F, -5.0F, 1, 9, 1, 0.0F);
         
-        this.RightPauldron2 = new ModelRenderer(this, 0, 91);
-        this.RightPauldron2.setRotationPoint(0.0F, 0.0F, 0.0F);
-        this.RightPauldron2.addBox(-4.5F, -3.0F, -2.5F, 6, 2, 5, 0.0F);
-        this.setRotateAngle(RightPauldron2, 0.0F, 0.0F, -0.17453292519943295F);
+        RightPauldron2 = new ModelRenderer(this, 0, 91);
+        RightPauldron2.setRotationPoint(0.0F, 0.0F, 0.0F);
+        RightPauldron2.addBox(-4.5F, -3.0F, -2.5F, 6, 2, 5, 0.0F);
+        setRotateAngle(RightPauldron2, 0.0F, 0.0F, -0.17453292519943295F);
         
-        this.HelmetFrontLeft2_1 = new ModelRenderer(this, 0, 64);
-        this.HelmetFrontLeft2_1.setRotationPoint(0.0F, 0.0F, 0.0F);
-        this.HelmetFrontLeft2_1.addBox(-3.0F, -5.0F, -5.0F, 1, 1, 1, 0.0F);
+        HelmetFrontLeft2_1 = new ModelRenderer(this, 0, 64);
+        HelmetFrontLeft2_1.setRotationPoint(0.0F, 0.0F, 0.0F);
+        HelmetFrontLeft2_1.addBox(-3.0F, -5.0F, -5.0F, 1, 1, 1, 0.0F);
         
-        this.TorsoStomach = new ModelRenderer(this, 40, 77);
-        this.TorsoStomach.setRotationPoint(0.0F, 0.0F, 0.0F);
-        this.TorsoStomach.addBox(-4.0F, 7.0F, -2.5F, 8, 5, 5, 0.0F);
+        TorsoStomach = new ModelRenderer(this, 40, 77);
+        TorsoStomach.setRotationPoint(0.0F, 0.0F, 0.0F);
+        TorsoStomach.addBox(-4.0F, 7.0F, -2.5F, 8, 5, 5, 0.0F);
         
-        this.RightPauldron1 = new ModelRenderer(this, 0, 81);
-        this.RightPauldron1.setRotationPoint(0.0F, 0.0F, 0.0F);
-        this.RightPauldron1.addBox(-3.5F, -2.5F, -2.5F, 5, 5, 5, 0.0F);
+        RightPauldron1 = new ModelRenderer(this, 0, 81);
+        RightPauldron1.setRotationPoint(0.0F, 0.0F, 0.0F);
+        RightPauldron1.addBox(-3.5F, -2.5F, -2.5F, 5, 5, 5, 0.0F);
         
-        this.LeftPauldron2 = new ModelRenderer(this, 0, 91);
-        this.LeftPauldron2.setRotationPoint(0.0F, 0.0F, 0.0F);
-        this.LeftPauldron2.addBox(-1.5F, -3.0F, -2.5F, 6, 2, 5, 0.0F);
-        this.setRotateAngle(LeftPauldron2, 0.0F, 0.0F, 0.17453292519943295F);
+        LeftPauldron2 = new ModelRenderer(this, 0, 91);
+        LeftPauldron2.setRotationPoint(0.0F, 0.0F, 0.0F);
+        LeftPauldron2.addBox(-1.5F, -3.0F, -2.5F, 6, 2, 5, 0.0F);
+        setRotateAngle(LeftPauldron2, 0.0F, 0.0F, 0.17453292519943295F);
         
-        this.TorsoChest = new ModelRenderer(this, 40, 64);
-        this.TorsoChest.setRotationPoint(0.0F, 0.0F, 0.0F);
-        this.TorsoChest.addBox(-4.0F, 0.0F, -3.0F, 8, 7, 6, 0.0F);
+        TorsoChest = new ModelRenderer(this, 40, 64);
+        TorsoChest.setRotationPoint(0.0F, 0.0F, 0.0F);
+        TorsoChest.addBox(-4.0F, 0.0F, -3.0F, 8, 7, 6, 0.0F);
         
-        this.LeftPauldron1.setRotationPoint(0.0F, 0.0F, 0.0F);
-        this.LeftPauldron1.addBox(-1.5F, -2.5F, -2.5F, 5, 5, 5, 0.0F);
+        LeftPauldron1.setRotationPoint(0.0F, 0.0F, 0.0F);
+        LeftPauldron1.addBox(-1.5F, -2.5F, -2.5F, 5, 5, 5, 0.0F);
         
-        this.HelmetRight1 = new ModelRenderer(this, 0, 65);
-        this.HelmetRight1.setRotationPoint(0.0F, 0.0F, 0.0F);
-        this.HelmetRight1.addBox(-5.0F, -8.0F, -4.0F, 1, 6, 8, 0.0F);
+        HelmetRight1 = new ModelRenderer(this, 0, 65);
+        HelmetRight1.setRotationPoint(0.0F, 0.0F, 0.0F);
+        HelmetRight1.addBox(-5.0F, -8.0F, -4.0F, 1, 6, 8, 0.0F);
         
-        this.HelmetBack = new ModelRenderer(this, 10, 65);
-        this.HelmetBack.setRotationPoint(0.0F, 0.0F, 0.0F);
-        this.HelmetBack.addBox(-4.0F, -8.0F, 4.0F, 8, 6, 1, 0.0F);
+        HelmetBack = new ModelRenderer(this, 10, 65);
+        HelmetBack.setRotationPoint(0.0F, 0.0F, 0.0F);
+        HelmetBack.addBox(-4.0F, -8.0F, 4.0F, 8, 6, 1, 0.0F);
         
-        this.HelmetLeft2 = new ModelRenderer(this, 0, 67);
-        this.HelmetLeft2.setRotationPoint(0.0F, 0.0F, 0.0F);
-        this.HelmetLeft2.addBox(4.0F, -2.0F, -4.0F, 1, 2, 3, 0.0F);
+        HelmetLeft2 = new ModelRenderer(this, 0, 67);
+        HelmetLeft2.setRotationPoint(0.0F, 0.0F, 0.0F);
+        HelmetLeft2.addBox(4.0F, -2.0F, -4.0F, 1, 2, 3, 0.0F);
         
-        this.HelmetFrontMid = new ModelRenderer(this, 0, 64);
-        this.HelmetFrontMid.setRotationPoint(0.0F, 0.0F, 0.0F);
-        this.HelmetFrontMid.addBox(-0.5F, -5.0F, -5.0F, 1, 2, 1, 0.0F);
+        HelmetFrontMid = new ModelRenderer(this, 0, 64);
+        HelmetFrontMid.setRotationPoint(0.0F, 0.0F, 0.0F);
+        HelmetFrontMid.addBox(-0.5F, -5.0F, -5.0F, 1, 2, 1, 0.0F);
         
-        this.HelmetFrontLeft2 = new ModelRenderer(this, 0, 64);
-        this.HelmetFrontLeft2.setRotationPoint(0.0F, 0.0F, 0.0F);
-        this.HelmetFrontLeft2.addBox(2.0F, -5.0F, -5.0F, 1, 1, 1, 0.0F);
+        HelmetFrontLeft2 = new ModelRenderer(this, 0, 64);
+        HelmetFrontLeft2.setRotationPoint(0.0F, 0.0F, 0.0F);
+        HelmetFrontLeft2.addBox(2.0F, -5.0F, -5.0F, 1, 1, 1, 0.0F);
         
-        this.HelmetFrontRight1 = new ModelRenderer(this, 34, 70);
-        this.HelmetFrontRight1.setRotationPoint(0.0F, 0.0F, 0.0F);
-        this.HelmetFrontRight1.addBox(-4.0F, -8.0F, -5.0F, 1, 9, 1, 0.0F);
+        HelmetFrontRight1 = new ModelRenderer(this, 34, 70);
+        HelmetFrontRight1.setRotationPoint(0.0F, 0.0F, 0.0F);
+        HelmetFrontRight1.addBox(-4.0F, -8.0F, -5.0F, 1, 9, 1, 0.0F);
         
-        this.HelmetLeft1 = new ModelRenderer(this, 0, 65);
-        this.HelmetLeft1.setRotationPoint(0.0F, 0.0F, 0.0F);
-        this.HelmetLeft1.addBox(4.0F, -8.0F, -4.0F, 1, 6, 8, 0.0F);
+        HelmetLeft1 = new ModelRenderer(this, 0, 65);
+        HelmetLeft1.setRotationPoint(0.0F, 0.0F, 0.0F);
+        HelmetLeft1.addBox(4.0F, -8.0F, -4.0F, 1, 6, 8, 0.0F);
         
-        this.HelmetFrontTop = new ModelRenderer(this, 28, 64);
-        this.HelmetFrontTop.setRotationPoint(0.0F, 0.0F, 0.0F);
-        this.HelmetFrontTop.addBox(-3.0F, -8.0F, -5.0F, 6, 3, 1, 0.0F);
-        this.setRotateAngle(HelmetFrontTop, 0.0F, 0.010821041362364843F, 0.0F);
+        HelmetFrontTop = new ModelRenderer(this, 28, 64);
+        HelmetFrontTop.setRotationPoint(0.0F, 0.0F, 0.0F);
+        HelmetFrontTop.addBox(-3.0F, -8.0F, -5.0F, 6, 3, 1, 0.0F);
+        setRotateAngle(HelmetFrontTop, 0.0F, 0.010821041362364843F, 0.0F);
         
-        this.LeftFoot = new ModelRenderer(this, 40, 93);
-        this.LeftFoot.setRotationPoint(0.0F, 0.0F, 0.0F);
-        this.LeftFoot.addBox(-1.8F, 6.0F, -3.0F, 4, 6, 6, 0.0F);
+        LeftFoot = new ModelRenderer(this, 40, 93);
+        LeftFoot.setRotationPoint(0.0F, 0.0F, 0.0F);
+        LeftFoot.addBox(-1.8F, 6.0F, -3.0F, 4, 6, 6, 0.0F);
         
-        this.HelmetRight2 = new ModelRenderer(this, 0, 67);
-        this.HelmetRight2.setRotationPoint(0.0F, 0.0F, 0.0F);
-        this.HelmetRight2.addBox(-5.0F, -2.0F, -4.0F, 1, 2, 3, 0.0F);
+        HelmetRight2 = new ModelRenderer(this, 0, 67);
+        HelmetRight2.setRotationPoint(0.0F, 0.0F, 0.0F);
+        HelmetRight2.addBox(-5.0F, -2.0F, -4.0F, 1, 2, 3, 0.0F);
         
-        this.RightLegGuard = new ModelRenderer(this, 22, 82);
-        this.RightLegGuard.setRotationPoint(0.0F, 0.0F, 0.0F);
-        this.RightLegGuard.addBox(-2.1F, -2.0F, -2.5F, 4, 14, 5, 0.0F);
-        
-        
+        RightLegGuard = new ModelRenderer(this, 22, 82);
+        RightLegGuard.setRotationPoint(0.0F, 0.0F, 0.0F);
+        RightLegGuard.addBox(-2.1F, -2.0F, -2.5F, 4, 14, 5, 0.0F);
         
         
         
-        if(isHelmet)
-        {
-        	this.bipedHead.addChild(this.HelmetTop);
-        	this.bipedHead.addChild(this.HelmetFrontLeft1);
-        	this.bipedHead.addChild(this.HelmetFrontLeft2_1);
-	        this.bipedHead.addChild(this.HelmetRight1);
-	        this.bipedHead.addChild(this.HelmetBack);
-	        this.bipedHead.addChild(this.HelmetLeft2);
-	        this.bipedHead.addChild(this.HelmetFrontMid);
-	        this.bipedHead.addChild(this.HelmetFrontLeft2);
-	        this.bipedHead.addChild(this.HelmetFrontRight1);
-	        this.bipedHead.addChild(this.HelmetLeft1);
-	        this.bipedHead.addChild(this.HelmetFrontTop);
-	        this.bipedHead.addChild(this.HelmetRight2);
-        }
-	        
-        if(isChest)
-        {
-	        this.bipedBody.addChild(this.TorsoStomach);
-	        this.bipedBody.addChild(this.TorsoChest);
-	        
-	        this.bipedRightArm.addChild(this.RightPauldron1);
-	        this.bipedRightArm.addChild(this.RightPauldron2);
-	        
-	        this.bipedLeftArm.addChild(this.LeftPauldron1);
-	        this.bipedLeftArm.addChild(this.LeftPauldron2);
-        }
-	        
-        if(isLegs)
-        {
-	        this.bipedRightLeg.addChild(this.RightLegGuard);
-	        this.bipedLeftLeg.addChild(this.LeftLegGuard);
-        }
-	    
-        if(isBoots)
-        {
-	        this.bipedRightLeg.addChild(this.RightFoot);
-	        this.bipedLeftLeg.addChild(this.LeftFoot);
-        }
+        
+        
+        bipedHead.addChild(HelmetTop);
+        bipedHead.addChild(HelmetFrontLeft1);
+        bipedHead.addChild(HelmetFrontLeft2_1);
+        bipedHead.addChild(HelmetRight1);
+        bipedHead.addChild(HelmetBack);
+        bipedHead.addChild(HelmetLeft2);
+        bipedHead.addChild(HelmetFrontMid);
+        bipedHead.addChild(HelmetFrontLeft2);
+        bipedHead.addChild(HelmetFrontRight1);
+        bipedHead.addChild(HelmetLeft1);
+        bipedHead.addChild(HelmetFrontTop);
+        bipedHead.addChild(HelmetRight2);
+    	
+        bipedBody.addChild(TorsoStomach);
+        bipedBody.addChild(TorsoChest);
+        
+        bipedRightArm.addChild(RightPauldron1);
+        bipedRightArm.addChild(RightPauldron2);
+        
+        bipedLeftArm.addChild(LeftPauldron1);
+        bipedLeftArm.addChild(LeftPauldron2);
+        
+        bipedRightLeg.addChild(RightLegGuard);
+        bipedLeftLeg.addChild(LeftLegGuard);
+        
+        bipedRightLeg.addChild(RightFoot);
+        bipedLeftLeg.addChild(LeftFoot);
     }
     
     @Override
-    public void render(Entity entity, float f, float f1, float f2, float f3, float f4, float f5)
+    public void render(@Nullable Entity entity, float f, float f1, float f2, float f3, float f4, float f5)
     {
-    	super.render(entity, f, f1, f2, f3, f4, f5);
-    	setRotationAngles(f, f1, f2, f3, f4, f5, entity);
+    	bipedHead.showModel = slot == EntityEquipmentSlot.HEAD;
+    	
+    	bipedBody.showModel = slot == EntityEquipmentSlot.CHEST;
+    	bipedRightArm.showModel = slot == EntityEquipmentSlot.CHEST;
+    	bipedLeftArm.showModel = slot == EntityEquipmentSlot.CHEST;
+    	
+    	bipedRightLeg.showModel = slot == EntityEquipmentSlot.LEGS;
+    	bipedLeftLeg.showModel = slot == EntityEquipmentSlot.LEGS;
+    	
+        bipedRightLeg.showModel = slot == EntityEquipmentSlot.FEET;
+        bipedLeftLeg.showModel = slot == EntityEquipmentSlot.FEET;
+        
+        bipedHeadwear.showModel = false;
+        
+        super.render(entity, f, f1, f2, f3, f4, f5);
     }
     
     public void setRotateAngle(ModelRenderer modelRenderer, float x, float y, float z)
