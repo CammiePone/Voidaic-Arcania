af0930e1fabd2e81dadef1e82086132f8b540e8d
Cammie <inazuma261@gmail.com>
2018-07-24 11:36:05 -0400

    Figured out the leggings issue

--- src/main/java/com/camellias/voidaicarcania/items/armour/ArmourMythril.java
+++ src/main/java/com/camellias/voidaicarcania/items/armour/ArmourMythril.java
@@ -31,33 +31,78 @@ public class ArmourMythril extends ItemArmor implements IHasModel
 	@SideOnly(Side.CLIENT)
 	public ModelBiped getArmorModel(EntityLivingBase entity, ItemStack stack, EntityEquipmentSlot slot, ModelBiped biped)
 	{
-		ModelMythrilArmour armourModel = ModelMythrilArmour.INSTANCE;
-		
 		if(stack != null)
 		{
 			if(stack.getItem() instanceof ItemArmor)
 			{
-				armourModel.bipedHead.showModel = slot == EntityEquipmentSlot.HEAD;
-				armourModel.bipedHeadwear.showModel = slot == EntityEquipmentSlot.HEAD;
+				if(slot == EntityEquipmentSlot.HEAD)
+				{
+					ModelMythrilArmour armourModel = ModelMythrilArmour.INSTANCE_HEAD;
+					
+					armourModel.bipedHead.showModel = slot == EntityEquipmentSlot.HEAD;
+					armourModel.bipedHeadwear.showModel = slot == EntityEquipmentSlot.HEAD;
+					
+					armourModel.isSneak = biped.isSneak;
+					armourModel.isRiding = biped.isRiding;
+					armourModel.isChild = biped.isChild;
+					armourModel.rightArmPose = biped.rightArmPose;
+					armourModel.leftArmPose = biped.leftArmPose;
+					
+					return armourModel;
+				}
 				
-				armourModel.bipedBody.showModel = (slot == EntityEquipmentSlot.CHEST) || (slot == EntityEquipmentSlot.CHEST);
-				armourModel.bipedRightArm.showModel = slot == EntityEquipmentSlot.CHEST;
-				armourModel.bipedLeftArm.showModel = slot == EntityEquipmentSlot.CHEST;
+				if(slot == EntityEquipmentSlot.CHEST)
+				{
+					ModelMythrilArmour armourModel = ModelMythrilArmour.INSTANCE_CHEST;
+					
+					armourModel.bipedBody.showModel = slot == EntityEquipmentSlot.CHEST;
+					armourModel.bipedRightArm.showModel = slot == EntityEquipmentSlot.CHEST;
+					armourModel.bipedLeftArm.showModel = slot == EntityEquipmentSlot.CHEST;
+					
+					armourModel.isSneak = biped.isSneak;
+					armourModel.isRiding = biped.isRiding;
+					armourModel.isChild = biped.isChild;
+					armourModel.rightArmPose = biped.rightArmPose;
+					armourModel.leftArmPose = biped.leftArmPose;
+					
+					return armourModel;
+				}
 				
-				armourModel.bipedRightLeg.showModel = (slot == EntityEquipmentSlot.LEGS) || (slot == EntityEquipmentSlot.FEET);
-				armourModel.bipedLeftLeg.showModel = (slot == EntityEquipmentSlot.LEGS) || (slot == EntityEquipmentSlot.FEET);
+				if(slot == EntityEquipmentSlot.LEGS)
+				{
+					ModelMythrilArmour armourModel = ModelMythrilArmour.INSTANCE_LEGS;
+					
+					armourModel.bipedRightLeg.showModel = slot == EntityEquipmentSlot.LEGS;
+					armourModel.bipedLeftLeg.showModel = slot == EntityEquipmentSlot.LEGS;
 
-				armourModel.isSneak = biped.isSneak;
-				armourModel.isRiding = biped.isRiding;
-				armourModel.isChild = biped.isChild;
-				armourModel.rightArmPose = biped.rightArmPose;
-				armourModel.leftArmPose = biped.leftArmPose;
+					armourModel.isSneak = biped.isSneak;
+					armourModel.isRiding = biped.isRiding;
+					armourModel.isChild = biped.isChild;
+					armourModel.rightArmPose = biped.rightArmPose;
+					armourModel.leftArmPose = biped.leftArmPose;
+					
+					return armourModel;
+				}
+				
+				if(slot == EntityEquipmentSlot.FEET)
+				{
+					ModelMythrilArmour armourModel = ModelMythrilArmour.INSTANCE_BOOTS;
+					
+					armourModel.bipedRightLeg.showModel = slot == EntityEquipmentSlot.FEET;
+					armourModel.bipedLeftLeg.showModel = slot == EntityEquipmentSlot.FEET;
 
-				return armourModel;
+					armourModel.isSneak = biped.isSneak;
+					armourModel.isRiding = biped.isRiding;
+					armourModel.isChild = biped.isChild;
+					armourModel.rightArmPose = biped.rightArmPose;
+					armourModel.leftArmPose = biped.leftArmPose;
+					
+					return armourModel;
+				}
 			}
 		}
 		
-		return armourModel;
+		return null;
 	}
 	
 	@Override
--- src/main/java/com/camellias/voidaicarcania/items/armour/models/ModelMythrilArmour.java
+++ src/main/java/com/camellias/voidaicarcania/items/armour/models/ModelMythrilArmour.java
@@ -6,7 +6,10 @@ import net.minecraft.entity.Entity;
 
 public class ModelMythrilArmour extends ModelBiped
 {
-	public static ModelMythrilArmour INSTANCE = new ModelMythrilArmour();
+	public static ModelMythrilArmour INSTANCE_HEAD = new ModelMythrilArmour(true, false, false, false);
+	public static ModelMythrilArmour INSTANCE_CHEST = new ModelMythrilArmour(false, true, false, false);
+	public static ModelMythrilArmour INSTANCE_LEGS = new ModelMythrilArmour(false, false, true, false);
+	public static ModelMythrilArmour INSTANCE_BOOTS = new ModelMythrilArmour(false, false, false, true);
 	
     private ModelRenderer HelmetLeft1;
     private ModelRenderer HelmetLeft2;
@@ -36,16 +39,16 @@ public class ModelMythrilArmour extends ModelBiped
     private ModelRenderer LeftLegGuard;
     private ModelRenderer LeftFoot;
     
-    public ModelMythrilArmour()
+    public ModelMythrilArmour(boolean isHelm, boolean isChest, boolean isLegs, boolean isBoots)
     {
         textureWidth = 64;
         textureHeight = 128;
         
-        HelmetTop = new ModelRenderer(this, 10, 72);
+        HelmetTop = new ModelRenderer(this, 0, 79);
         HelmetTop.setRotationPoint(0.0F, 0.0F, 0.0F);
         HelmetTop.addBox(-4.0F, -9.0F, -4.0F, 8, 1, 8, 0.0F);
         
-        HelmetBack = new ModelRenderer(this, 10, 65);
+        HelmetBack = new ModelRenderer(this, 42, 106);
         HelmetBack.setRotationPoint(0.0F, 0.0F, 0.0F);
         HelmetBack.addBox(-4.0F, -8.0F, 4.0F, 8, 6, 1, 0.0F);
         
@@ -53,7 +56,7 @@ public class ModelMythrilArmour extends ModelBiped
         HelmetRight1.setRotationPoint(0.0F, 0.0F, 0.0F);
         HelmetRight1.addBox(-5.0F, -8.0F, -4.0F, 1, 6, 8, 0.0F);
         
-        HelmetRight2 = new ModelRenderer(this, 0, 67);
+        HelmetRight2 = new ModelRenderer(this, 22, 72);
         HelmetRight2.setRotationPoint(0.0F, 0.0F, 0.0F);
         HelmetRight2.addBox(-5.0F, -2.0F, -4.0F, 1, 2, 3, 0.0F);
         
@@ -61,32 +64,32 @@ public class ModelMythrilArmour extends ModelBiped
         HelmetLeft1.setRotationPoint(0.0F, 0.0F, 0.0F);
         HelmetLeft1.addBox(4.0F, -8.0F, -4.0F, 1, 6, 8, 0.0F);
         
-        HelmetLeft2 = new ModelRenderer(this, 0, 67);
+        HelmetLeft2 = new ModelRenderer(this, 22, 72);
         HelmetLeft2.setRotationPoint(0.0F, 0.0F, 0.0F);
         HelmetLeft2.addBox(4.0F, -2.0F, -4.0F, 1, 2, 3, 0.0F);
         
-        HelmetFrontTop = new ModelRenderer(this, 28, 64);
+        HelmetFrontTop = new ModelRenderer(this, 0, 89);
         HelmetFrontTop.setRotationPoint(0.0F, 0.0F, 0.0F);
         HelmetFrontTop.addBox(-3.0F, -8.0F, -5.0F, 6, 3, 1, 0.0F);
         setRotateAngle(HelmetFrontTop, 0.0F, 0.010821041362364843F, 0.0F);
         
-        HelmetFrontMid = new ModelRenderer(this, 0, 64);
+        HelmetFrontMid = new ModelRenderer(this, 18, 72);
         HelmetFrontMid.setRotationPoint(0.0F, 0.0F, 0.0F);
         HelmetFrontMid.addBox(-0.5F, -5.0F, -5.0F, 1, 2, 1, 0.0F);
         
-        HelmetFrontRight1 = new ModelRenderer(this, 34, 101);
+        HelmetFrontRight1 = new ModelRenderer(this, 33, 78);
         HelmetFrontRight1.setRotationPoint(0.0F, 0.0F, 0.0F);
         HelmetFrontRight1.addBox(-4.0F, -8.0F, -5.0F, 1, 9, 1, 0.0F);
         
-        HelmetFrontRight2 = new ModelRenderer(this, 0, 64);
+        HelmetFrontRight2 = new ModelRenderer(this, 18, 72);
         HelmetFrontRight2.setRotationPoint(0.0F, 0.0F, 0.0F);
         HelmetFrontRight2.addBox(-3.0F, -5.0F, -5.0F, 1, 1, 1, 0.0F);
         
-        HelmetFrontLeft1 = new ModelRenderer(this, 34, 101);
+        HelmetFrontLeft1 = new ModelRenderer(this, 33, 78);
         HelmetFrontLeft1.setRotationPoint(0.0F, 0.0F, 0.0F);
         HelmetFrontLeft1.addBox(3.0F, -8.0F, -5.0F, 1, 9, 1, 0.0F);
         
-        HelmetFrontLeft2 = new ModelRenderer(this, 0, 64);
+        HelmetFrontLeft2 = new ModelRenderer(this, 18, 72);
         HelmetFrontLeft2.setRotationPoint(0.0F, 0.0F, 0.0F);
         HelmetFrontLeft2.addBox(2.0F, -5.0F, -5.0F, 1, 1, 1, 0.0F);
         
@@ -106,11 +109,11 @@ public class ModelMythrilArmour extends ModelBiped
         
         
         
-        RightPauldron1 = new ModelRenderer(this, 0, 81);
+        RightPauldron1 = new ModelRenderer(this, 0, 93);
         RightPauldron1.setRotationPoint(0.0F, 0.0F, 0.0F);
         RightPauldron1.addBox(-3.5F, -2.5F, -2.5F, 5, 5, 5, 0.0F);
         
-        RightPauldron2 = new ModelRenderer(this, 0, 91);
+        RightPauldron2 = new ModelRenderer(this, 0, 103);
         RightPauldron2.setRotationPoint(0.0F, 0.0F, 0.0F);
         RightPauldron2.addBox(-4.5F, -3.0F, -2.5F, 6, 2, 5, 0.0F);
         setRotateAngle(RightPauldron2, 0.0F, 0.0F, -0.17453292519943295F);
@@ -119,11 +122,11 @@ public class ModelMythrilArmour extends ModelBiped
         
         
         
-        LeftPauldron1 = new ModelRenderer(this, 0, 81);
+        LeftPauldron1 = new ModelRenderer(this, 0, 93);
         LeftPauldron1.setRotationPoint(0.0F, 0.0F, 0.0F);
         LeftPauldron1.addBox(-1.5F, -2.5F, -2.5F, 5, 5, 5, 0.0F);
         
-        LeftPauldron2 = new ModelRenderer(this, 0, 91);
+        LeftPauldron2 = new ModelRenderer(this, 0, 103);
         LeftPauldron2.setRotationPoint(0.0F, 0.0F, 0.0F);
         LeftPauldron2.addBox(-1.5F, -3.0F, -2.5F, 6, 2, 5, 0.0F);
         setRotateAngle(LeftPauldron2, 0.0F, 0.0F, 0.17453292519943295F);
@@ -132,11 +135,11 @@ public class ModelMythrilArmour extends ModelBiped
         
         
         
-        RightLegGuard = new ModelRenderer(this, 22, 82);
+        RightLegGuard = new ModelRenderer(this, 24, 109);
         RightLegGuard.setRotationPoint(0.0F, 0.0F, 0.0F);
         RightLegGuard.addBox(-2.1F, -2.0F, -2.5F, 4, 14, 5, 0.0F);
         
-        LeftLegGuard = new ModelRenderer(this, 22, 82);
+        LeftLegGuard = new ModelRenderer(this, 24, 109);
         LeftLegGuard.setRotationPoint(0.0F, 0.0F, 0.0F);
         LeftLegGuard.addBox(-1.9F, -2.0F, -2.5F, 4, 14, 5, 0.0F);
         
@@ -156,33 +159,46 @@ public class ModelMythrilArmour extends ModelBiped
         
         
         
-        bipedHead.addChild(HelmetTop);
-        bipedHead.addChild(HelmetFrontLeft1);
-        bipedHead.addChild(HelmetFrontRight2);
-        bipedHead.addChild(HelmetRight1);
-        bipedHead.addChild(HelmetBack);
-        bipedHead.addChild(HelmetLeft2);
-        bipedHead.addChild(HelmetFrontMid);
-        bipedHead.addChild(HelmetFrontLeft2);
-        bipedHead.addChild(HelmetFrontRight1);
-        bipedHead.addChild(HelmetLeft1);
-        bipedHead.addChild(HelmetFrontTop);
-        bipedHead.addChild(HelmetRight2);
-    	
-        bipedBody.addChild(TorsoStomach);
-        bipedBody.addChild(TorsoChest);
+        if(isHelm)
+        {
+	        bipedHead.addChild(HelmetTop);
+	        bipedHead.addChild(HelmetFrontLeft1);
+	        bipedHead.addChild(HelmetFrontRight2);
+	        bipedHead.addChild(HelmetRight1);
+	        bipedHead.addChild(HelmetBack);
+	        bipedHead.addChild(HelmetLeft2);
+	        bipedHead.addChild(HelmetFrontMid);
+	        bipedHead.addChild(HelmetFrontLeft2);
+	        bipedHead.addChild(HelmetFrontRight1);
+	        bipedHead.addChild(HelmetLeft1);
+	        bipedHead.addChild(HelmetFrontTop);
+	        bipedHead.addChild(HelmetRight2);
+        }
         
-        bipedRightArm.addChild(RightPauldron1);
-        bipedRightArm.addChild(RightPauldron2);
         
-        bipedLeftArm.addChild(LeftPauldron1);
-        bipedLeftArm.addChild(LeftPauldron2);
+        if(isChest)
+        {
+	        bipedBody.addChild(TorsoStomach);
+	        bipedBody.addChild(TorsoChest);
+	        
+	        bipedRightArm.addChild(RightPauldron1);
+	        bipedRightArm.addChild(RightPauldron2);
+	        
+	        bipedLeftArm.addChild(LeftPauldron1);
+	        bipedLeftArm.addChild(LeftPauldron2);
+        }
         
-        bipedRightLeg.addChild(RightLegGuard);
-        bipedLeftLeg.addChild(LeftLegGuard);
+        if(isLegs)
+        {
+	        bipedRightLeg.addChild(RightLegGuard);
+	        bipedLeftLeg.addChild(LeftLegGuard);
+        }
         
-        bipedRightLeg.addChild(RightFoot);
-        bipedLeftLeg.addChild(LeftFoot);
+        if(isBoots)
+        {
+	        bipedRightLeg.addChild(RightFoot);
+	        bipedLeftLeg.addChild(LeftFoot);
+        }
     }
     
     @Override
