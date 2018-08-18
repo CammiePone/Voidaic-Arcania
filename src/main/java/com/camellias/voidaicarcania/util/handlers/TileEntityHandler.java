69d34be36e6c3ea75f59026000c7a421cf939813
Cammie <inazuma261@gmail.com>
2018-08-18 16:46:39 -0400

    Registered the other two tile entities because I'm an idiot and never did

--- src/main/java/com/camellias/voidaicarcania/util/handlers/TileEntityHandler.java
+++ src/main/java/com/camellias/voidaicarcania/util/handlers/TileEntityHandler.java
@@ -1,6 +1,8 @@
 package com.camellias.voidaicarcania.util.handlers;
 
 import com.camellias.voidaicarcania.blocks.blockaltar.TileEntityAltar;
+import com.camellias.voidaicarcania.blocks.blockbotanyaltar.TileEntityBotanyAltar;
+import com.camellias.voidaicarcania.blocks.blockmortalcentrifuge.TileEntityMortalCentrifuge;
 
 import net.minecraftforge.fml.common.registry.GameRegistry;
 
@@ -9,5 +11,7 @@ public class TileEntityHandler
 	public static void registerTileEntities()
 	{
 		GameRegistry.registerTileEntity(TileEntityAltar.class, "altar");
+		GameRegistry.registerTileEntity(TileEntityBotanyAltar.class, "botanical_altar");
+		GameRegistry.registerTileEntity(TileEntityMortalCentrifuge.class, "mortal_centrifuge");
 	}
 }
