1670ffb13aec211b98fb4bcd53603091f9d3c935
Cammie <inazuma261@gmail.com>
2019-01-07 15:08:33 -0500

    Added Dan to the Donator list

--- src/main/java/com/camellias/voidaicarcania/kofi/Donators.java
+++ src/main/java/com/camellias/voidaicarcania/kofi/Donators.java
@@ -9,4 +9,11 @@ public class Donators
 	public static final List<UUID> DONATOR_ID = new ArrayList<UUID>();
 	
 	public static final UUID TigersHideout = UUID.fromString("01155b6d-2338-4513-909c-224ddcc0214f");
+	public static final UUID DanTheDetermined = UUID.fromString("3810639e-b52f-4df1-ad4b-748236bf77b9");
+	
+	public static void addDonators()
+	{
+		DONATOR_ID.add(TigersHideout);
+		DONATOR_ID.add(DanTheDetermined);
+	}
 }
