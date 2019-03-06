05968c68c829082a39498e231fd8dbe7e9ff14c1
Cammie <inazuma261@gmail.com>
2019-03-05 21:58:20 -0500

    Still crashes, but this is supposedly better

--- src/main/java/com/camellias/voidaicarcania/handlers/EventHandler.java
+++ src/main/java/com/camellias/voidaicarcania/handlers/EventHandler.java
@@ -1,32 +1,56 @@
 package com.camellias.voidaicarcania.handlers;
 
+import java.util.HashMap;
+
+import net.minecraft.block.state.IBlockState;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.World;
+import net.minecraft.world.chunk.Chunk;
+import net.minecraft.world.chunk.storage.ExtendedBlockStorage;
 import net.minecraftforge.event.terraingen.PopulateChunkEvent;
-import net.minecraftforge.fml.common.eventhandler.EventPriority;
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
 
 public class EventHandler
 {
-	@SubscribeEvent(priority = EventPriority.HIGHEST)
-	public void onChunkPopulate(PopulateChunkEvent.Pre event)
+	@SubscribeEvent
+	public void onChunkPopulate(PopulateChunkEvent.Post event)
 	{
-		if(event.getWorld() != null)
+		HashMap<BlockPos, IBlockState> map = new HashMap<BlockPos, IBlockState>();
+		Chunk chunk = event.getWorld().getChunk(event.getChunkX(), event.getChunkZ());
+		
+		for(ExtendedBlockStorage storage : chunk.getBlockStorageArray())
 		{
-			World world = event.getWorld();
-			
-			for(int x = 0; x < 16; x++)
+			if(storage != null)
 			{
-			    for(int z = 0; z < 16; z++)
-			    {
-			        for(int y = 255; y >= 0; y--)
-			        {
-			        	BlockPos oldPos = new BlockPos(x * event.getChunkX(), y, z * event.getChunkZ());
-						BlockPos newPos = new BlockPos(x * event.getChunkX(), y + 16, z * event.getChunkZ());
-						
-						world.setBlockState(newPos, world.getBlockState(oldPos));
-			        }
-			    }
+				for(int x = 0; x < 16; x++)
+				{
+					for(int y = 0; y < 255 - 16; y++)
+					{
+						for(int z = 0; z < 16; z++)
+						{
+							BlockPos pos = new BlockPos(x, y, z);
+							map.put(pos, storage.get(x, y, z));
+						}        
+					}
+				}
+			}
+		}
+		
+		for(ExtendedBlockStorage storage : chunk.getBlockStorageArray())
+		{
+			if(storage != null)
+			{
+				for(int x = 0; x < 16; x++)
+				{
+					for(int y = 0; y < 255 - 16; y++)
+					{
+						for(int z = 0; z < 16; z++)
+						{
+							BlockPos pos = new BlockPos(x, y, z);
+							storage.set(pos.getX(), pos.getY() + 16, pos.getZ(), map.get(pos));
+						}        
+					}
+				}
 			}
 		}
 	}
