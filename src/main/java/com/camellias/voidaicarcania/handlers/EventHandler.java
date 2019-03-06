3753853d985c61292b39b849e06485f264a00c75
Cammie <inazuma261@gmail.com>
2019-03-05 23:01:42 -0500

    I think it's working. Now to pawn off the optimization to someone else

--- src/main/java/com/camellias/voidaicarcania/handlers/EventHandler.java
+++ src/main/java/com/camellias/voidaicarcania/handlers/EventHandler.java
@@ -3,6 +3,7 @@ package com.camellias.voidaicarcania.handlers;
 import java.util.HashMap;
 
 import net.minecraft.block.state.IBlockState;
+import net.minecraft.init.Blocks;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.World;
 import net.minecraft.world.chunk.Chunk;
@@ -15,42 +16,19 @@ public class EventHandler
 	@SubscribeEvent
 	public void onChunkPopulate(PopulateChunkEvent.Post event)
 	{
-		HashMap<BlockPos, IBlockState> map = new HashMap<BlockPos, IBlockState>();
 		Chunk chunk = event.getWorld().getChunk(event.getChunkX(), event.getChunkZ());
 		
-		for(ExtendedBlockStorage storage : chunk.getBlockStorageArray())
+		for(int x = 0; x < 16; x++)
 		{
-			if(storage != null)
+			for(int y = 0; y < 255 - 16; y++)
 			{
-				for(int x = 0; x < 16; x++)
+				for(int z = 0; z < 16; z++)
 				{
-					for(int y = 0; y < 255 - 16; y++)
-					{
-						for(int z = 0; z < 16; z++)
-						{
-							BlockPos pos = new BlockPos(x, y, z);
-							map.put(pos, storage.get(x, y, z));
-						}        
-					}
-				}
-			}
-		}
-		
-		for(ExtendedBlockStorage storage : chunk.getBlockStorageArray())
-		{
-			if(storage != null)
-			{
-				for(int x = 0; x < 16; x++)
-				{
-					for(int y = 0; y < 255 - 16; y++)
-					{
-						for(int z = 0; z < 16; z++)
-						{
-							BlockPos pos = new BlockPos(x, y, z);
-							storage.set(pos.getX(), pos.getY() + 16, pos.getZ(), map.get(pos));
-						}        
-					}
-				}
+					BlockPos pos = new BlockPos(x,y,z);
+					IBlockState state = chunk.getBlockState(pos);
+					chunk.setBlockState(pos, Blocks.AIR.getDefaultState());
+					chunk.setBlockState(pos.up(16), state);
+				}        
 			}
 		}
 	}
