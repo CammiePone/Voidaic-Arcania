dc54c8379e7d877b1f4bc95a5d1e94c988a16d23
Cammie <inazuma261@gmail.com>
2019-03-13 13:17:25 -0400

    Just a matter of finishing the texture, and making the item

--- src/main/java/com/camellias/voidaicarcania/client/renderer/hud/OverlayRenderer.java
+++ src/main/java/com/camellias/voidaicarcania/client/renderer/hud/OverlayRenderer.java
@@ -52,26 +52,58 @@ public class OverlayRenderer
 		GlStateManager.disableLighting();
 		
 		GlStateManager.color(1.0f, 1.0f, 1.0f, 1.0f);
+		GlStateManager.scale(0.75f, 0.75f, 0.75f);
 		GuiIngame gui = new GuiIngame(mc);
 		FontRenderer fontRenderer = gui.getFontRenderer();
 		TextureManager textureManager = this.mc.getTextureManager();
-		textureManager.bindTexture(TEXTURES);
-		
 		int x = 0;
 		int y = event.getResolution().getScaledHeight() - 47;
+		
+		textureManager.bindTexture(TEXTURES);
 		gui.drawTexturedModalRect(x + 5, y - 5, 0, 0, 240, 47);
+		
+		int chunkEssenceMeter = chunkEssenceMeter(100);
+		gui.drawTexturedModalRect(x + 16, y + 3, 0, 47, chunkEssenceMeter, 12);
+		
+		int chunkCorruptionMeter = chunkCorruptionMeter(100);
+		gui.drawTexturedModalRect(x + 134, y + 3, 140, 47, chunkCorruptionMeter, 12);
+		
+		int playerCorruptionMeter = playerCorruptionMeter(110);
+		gui.drawTexturedModalRect(x + 70, y + 22, 65, 60, playerCorruptionMeter, 12);
+		
 		if(this.mc.player.isSneaking())
 		{
 			x = 26;
 			y += 5;
-			x = fontRenderer.drawString("Chunk VE: ", x, y, 0xffffffff);
-			x = fontRenderer.drawString("" + chunkEssence, x, y, 0xffff0000);
-			x = fontRenderer.drawString("             Chunk VC: ", x, y, 0xffffffff);
-			x = fontRenderer.drawString("" + chunkCorruption, x, y, 0xffff0000);
-			y += 20;
+			x = fontRenderer.drawString("Chunk VE: ", x, y, 0xffffff);
+			x = fontRenderer.drawString("" + chunkEssence, x, y, 0xffffff);
+			x = fontRenderer.drawString("             Chunk VC: ", x, y, 0xffffff);
+			x = fontRenderer.drawString("" + chunkCorruption, x, y, 0xffffff);
+			y += 19;
 			x = 77;
-			x = fontRenderer.drawString("Player VC: ", x, y, 0xffffffff);
-			x = fontRenderer.drawString("" + (playerCorruption), x, y, 0xffff0000);
+			x = fontRenderer.drawString("Player VC: ", x, y, 0xffffff);
+			x = fontRenderer.drawString("" + (playerCorruption), x, y, 0xffffff);
 		}
+	}
+	
+	private int chunkEssenceMeter(int pixels)
+	{
+		int i = this.chunkEssence;
+		int j = this.maxChunkEssence;
+		return i != 0 && j != 0 ? i * pixels / j : 0;
+	}
+	
+	private int chunkCorruptionMeter(int pixels)
+	{
+		int i = this.chunkCorruption;
+		int j = this.maxChunkCorruption;
+		return i != 0 && j != 0 ? i * pixels / j : 0;
+	}
+	
+	private int playerCorruptionMeter(int pixels)
+	{
+		int i = this.playerCorruption;
+		int j = this.maxPlayerCorruption;
+		return i != 0 && j != 0 ? i * pixels / j : 0;
 	}
 }
