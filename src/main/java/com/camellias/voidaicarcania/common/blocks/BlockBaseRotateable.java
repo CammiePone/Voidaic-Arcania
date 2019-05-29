89dc4200a9178bf5d48ae9a37e7cfcf9399173fc
Cammie <inazuma261@gmail.com>
2019-05-29 07:57:39 -0400

    Fixed whitewood logs

---
+++ src/main/java/com/camellias/voidaicarcania/common/blocks/BlockBaseRotateable.java
@@ -0,0 +1,33 @@
+package com.camellias.voidaicarcania.common.blocks;
+
+import com.camellias.voidaicarcania.Main;
+import com.camellias.voidaicarcania.Reference;
+import com.camellias.voidaicarcania.core.init.ModBlocks;
+import com.camellias.voidaicarcania.core.init.ModCreativeTabs;
+import com.camellias.voidaicarcania.core.init.ModItems;
+import com.camellias.voidaicarcania.core.util.IHasModel;
+
+import net.minecraft.block.BlockRotatedPillar;
+import net.minecraft.block.material.Material;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemBlock;
+
+public class BlockBaseRotateable extends BlockRotatedPillar implements IHasModel
+{
+	public BlockBaseRotateable(Material material, String name)
+	{
+		super(material);
+		this.setTranslationKey(Reference.MODID + "." + name);
+		this.setRegistryName(name);
+		this.setCreativeTab(ModCreativeTabs.VA_TAB_MAIN);
+		
+		ModBlocks.BLOCKS.add(this);
+		ModItems.ITEMS.add(new ItemBlock(this).setRegistryName(this.getRegistryName()));
+	}
+	
+	@Override
+	public void registerModels() 
+	{
+		Main.proxy.registerItemRenderer(Item.getItemFromBlock(this), 0, "inventory");
+	}
+}
--- src/main/java/com/camellias/voidaicarcania/common/blocks/plants/trees/BlockWhitewoodLog.java
+++ src/main/java/com/camellias/voidaicarcania/common/blocks/plants/trees/BlockWhitewoodLog.java
@@ -1,11 +1,11 @@
 package com.camellias.voidaicarcania.common.blocks.plants.trees;
 
-import com.camellias.voidaicarcania.common.blocks.BlockBaseGeneric;
+import com.camellias.voidaicarcania.common.blocks.BlockBaseRotateable;
 
 import net.minecraft.block.SoundType;
 import net.minecraft.block.material.Material;
 
-public class BlockWhitewoodLog extends BlockBaseGeneric
+public class BlockWhitewoodLog extends BlockBaseRotateable
 {
 	public BlockWhitewoodLog(Material material, String name)
 	{
